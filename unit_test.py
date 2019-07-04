3d66b43643115f98ebadbe670aab114498a005ab
SpicyChickenFLY <xixiayuchao@gmail.com>
2019-07-04 21:40:31 +0800

    update unittest

--- unit_test.py
+++ unit_test.py
@@ -6,9 +6,21 @@ Last Review: 2019/05/31
 
 import unittest
 
+from deck import Card, Deck
+
 class TestDeckFunctions(unittest.TestCase):
     def test_card_init(self):
-        pass
+        for value in range(14):
+            for suit in ['spade', 'club', 'heart', 'diamond']:
+                card = Card(value, suit, True)
+                card.flip()
+                card = Card(value, suit, False)
+                card.flip()
+                card.show()
+        card = Card(14, 'none', True)
+        card.show()
+        card = Card(15, 'none', True)
+        card.show()
 
     def test_deck_init(self):
         pass
@@ -18,4 +30,4 @@ class TestPlayerFunction(unittest.TestCase):
         pass
 
 if __name__ == "__main__":
-    pass
+    unittest.main()
